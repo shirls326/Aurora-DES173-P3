f0e05dfd96b0c3b4d28aac435722b248822146b5
Shirley Angos <shirleyangos@dyn223037.cc.lehigh.edu>
2024-12-02 16:34:23 -0500

    made many changes to format of signup to make responsiveness and fixed container issues

--- DES173FinalProject/src/main.jsx
+++ DES173FinalProject/src/main.jsx
@@ -13,6 +13,7 @@ import './index.css';
 import Landing from './pages/landing/Landing';
 import SignUpSerial from './pages/signupSerial/SignupSerial';
 import SignUp from './pages/signup/Signup';
+import OnboardingPref from './pages/onboardingPref/OnboardingPref';
 // import Login from './pages/login/login';
 
 
@@ -23,6 +24,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Landing />}/>
         <Route path="/SignUpSerial" element={<SignUpSerial />}/>
         <Route path="/SignUp" element={<SignUp />}/>
+        <Route path="/OnboardingPref" element={<OnboardingPref />}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
--- DES173FinalProject/src/pages/signup/Signup.jsx
+++ DES173FinalProject/src/pages/signup/Signup.jsx
@@ -4,9 +4,9 @@ import './Signup.css'
 export default function Signup() {
   return (
     <div className='SignUp container'>
-        <div className='innerContainer'>
-            <h1>Sign Up</h1>
-            <p>Enter your details below to create your <br/> account and get started</p>
+      <div className='innerContainer'>  
+          <h1>Sign Up</h1>
+          <p>Enter your details below to create your <br/> account and get started</p>
             <div className='SignUpInfo'>
               <div className='col'>
                 <div className='InputContainer'>
@@ -15,36 +15,35 @@ export default function Signup() {
                 </div>
                 <div className='InputContainer'>
                     <p>Phone Number</p>
-                    <input type = "text" placeholder='123-456-7899'></input>
+                    <input type = "text" placeholder='123-456-7890'></input>
                 </div>
                 <div className='InputContainer'>
                     <p>Birthday</p>
-                    <input type = "text" placeholder='123-456-7899'></input>
+                    <input type = "text" placeholder='01/01/1999'></input>
                 </div>
-
+            
 
               </div>
               <div className='col'>
                 <div className='InputContainer'>
                     <p>Email</p>
-                    <input type = "text" placeholder='John Doe'></input>
+                    <input type = "text" placeholder='johndoe@example.com'></input>
                 </div>
                 <div className='InputContainer'>
                     <p>Address</p>
-                    <input type = "text" placeholder='123-456-7899'></input>
+                    <input type = "text" placeholder='123 Example Street'></input>
                 </div>
                 <div className='InputContainer'>
                     <p>Password</p>
-                    <input type = "text" placeholder='123-456-7899'></input>
+                    <input type = "password" placeholder=''></input>
                 </div>
 
 
               </div>
                 
             </div>
-          
-
-
+            <button>Continue</button>
+        
         </div>
 
     </div>
